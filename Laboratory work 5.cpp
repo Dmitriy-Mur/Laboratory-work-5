abe2e3988a9638af1cf63e9ea8316de6f99c2b20
Dmitriy-Mur <148892171+Dmitriy-Mur@users.noreply.github.com>
2024-02-04 11:15:40 +0500

    Add files via upload

---
+++ Laboratory work 5.cpp
@@ -0,0 +1,42 @@
+﻿#include <iostream>
+using namespace std;
+const int A = 20;
+int arr_one[A];
+const int B = 5;
+const int C = A / B;
+int arr_two[B][C];
+
+void array_one()
+{
+    srand(time(0));
+    cout << "Исходный одномерный массив:\n";
+    for (int i = 0; i < A; i++)
+    {
+        arr_one[i] = (rand() % 100 - 50);
+        cout << arr_one[i] << " ";
+    }
+    cout << "\n";
+}
+
+void array_two(int a[])
+{
+    int index = 0;
+    cout << "Восстановленный двумерный массив:\n";
+    for (int i = 0; i < B; i++)
+    {
+        for (int j = 0; j < C; j++) 
+        {
+            arr_two[j][i] = a[index];
+            cout << arr_two[j][i] << "\t";
+            index++;
+        }
+        cout << "\n";
+    }
+}
+
+int main()
+{
+    setlocale(LC_ALL, "Rus");
+    array_one();
+    array_two(arr_one);
+}
